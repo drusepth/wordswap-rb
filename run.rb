4d2e7929524359bfebf6e10ed180c522c25b3f71
Andrew Brown <drusepth@gmail.com>
2021-08-27 01:02:05 -0700

    backing up old repo

--- run.rb
+++ run.rb
@@ -3,10 +3,9 @@ class WordswapService
     {
       reading_level:   {
         label: "Reading level",
-        value: reading_level(sentence),
-        method: ->(s) { reading_level(s) }
+        value: DactylMetric.reading_level(sentence),
+        method: ->(s) { DactylMetric.reading_level(s) }
       },
-
 
       number_of_words: word_count(sentence)
       #number_of_syllables: (FOR POETRY IDE)
@@ -35,11 +34,14 @@ class WordswapService
         puts "[#{replacement}] - #{new_sentence}\n\tScore: #{new_sentence_score}"
 
         if new_sentence_score == current_sentence_score
+          puts "Adding possible replacement #{word}=>#{replacement} of score #{new_sentence_score}"
+
           # Append to possible best-score options
           replacements << [i, word, replacement]
         elsif new_sentence_score > current_sentence_score
           # We have a new record, so clear the current possible options and add this one
           # todo minimal_swap flip > here
+          puts "Clearing replacements for everything less than #{current_sentence_score}"
           replacements = [[i, word, replacement]]
 
           # Set new record
@@ -48,24 +50,26 @@ class WordswapService
       end
     end.join(' ')
 
+    puts "Replacements\n\t"
+    puts replacements.inspect
+
     # Resolve replacements by picking 1 at random of best choices
     # tbd suggest all and let a consumer/user pick
-    puts replacements.inspect
     replacement = replacements.sample
     sentence_dup = sentence.dup.split(' ')
-    puts replacement.inspect
     sentence_dup[replacement[0]] = replacement[2]
     sentence_dup = sentence_dup.join(' ')
 
-    original_fitness = fitness_proc.call(sentence)
+    previous_fitness = fitness_proc.call(sentence)
     new_fitness      = fitness_proc.call(sentence_dup)
 
-    if new_fitness > original_fitness # maximizing
+    puts "Previous fitness = #{previous_fitness}"
+    if new_fitness > previous_fitness # maximizing
       # If we made an improvement with this swap, then we want to run the function
       # again with the new sentence -- until no new improvements are made. 
       sentence, _, score = maximal_swap(sentence_dup, fitness_proc, new_fitness)
 
-      if score > new_fitness
+      if score >= new_fitness
         # If our recursion found a better solution, return that
         [sentence, fitness_proc, score]
       else
@@ -91,11 +95,6 @@ class WordswapService
     words(sentence).count
   end
 
-  def self.reading_level(sentence)
-    # stub: more words = higher level
-    sentence.split(' ').count
-  end
-
   def self.thesaurus_results(word)
     # hit API or DB or something
     [word] + case word.downcase
@@ -111,6 +110,12 @@ class WordswapService
   end
 end
 
+class DactylMetric
+  def self.reading_level(sentence)
+    sentence.split(' ').count
+  end
+end
+
 puts "Enter a sentence to input:"
 sentence = gets.chomp
 sentence = "The quick, brown fox jumped over the lazy-ass dog." if sentence.empty?
@@ -119,7 +124,7 @@ puts "Metric scores: "
 puts WordswapService.metric_scores(sentence).inspect
 
 puts "Doing the swap for: more words"
-new_sentence, fitness, score = WordswapService.maximal_swap(sentence, ->(s) { WordswapService.reading_level(s) })
+new_sentence, fitness, score = WordswapService.maximal_swap(sentence, ->(s) { DactylMetric.reading_level(s) })
 
 puts "Original sentence: " + sentence.to_s + " (score=" + fitness.call(sentence).to_s + ")"
 puts "New sentence: " + new_sentence.to_s + " (score=" + score.to_s + ")"
